e242db9fce88cc6a05397427e9d3a2518b02038e
Basil Schneider <basil.schneider@cern.ch>
2016-06-17 09:21:29 -0500

    Comment out complete rootlogon script

--- .rootlogon.C
+++ .rootlogon.C
@@ -1,25 +1,28 @@
 {
-  // Set up FW Lite for automatic loading of CMS libraries
-  // and data formats.   As you may have other user-defined setup
-  // in your rootlogon.C, the CMS setup is executed only if the CMS
-  // environment is set up.
+    /*
+    // Set up FW Lite for automatic loading of CMS libraries
+    // and data formats.   As you may have other user-defined setup
+    // in your rootlogon.C, the CMS setup is executed only if the CMS
+    // environment is set up.
 
-  // To load FW lite, export the LOADFWLITE variable in bash
-  TString loadfwlite = getenv("LOADFWLITE");
-  TString cmsswbase = getenv("CMSSW_BASE");
+    // To load FW lite, export the LOADFWLITE variable in bash
+    TString loadfwlite = getenv("LOADFWLITE");
+    TString cmsswbase = getenv("CMSSW_BASE");
 
-  if (cmsswbase.Length() > 0 && loadfwlite.Length() > 0) {
+    if (cmsswbase.Length() > 0 && loadfwlite.Length() > 0) {
     //
     // The CMSSW environment is defined (this is true even for FW Lite)
     // so set up the rest.
     //
     cout << "Loading FW Lite setup." << endl;
     gSystem->Load("libFWCoreFWLite.so");
-    AutoLibraryLoader::enable();
+    //AutoLibraryLoader::enable();
+    FWLiteEnabler::enable();
     gSystem->Load("libDataFormatsFWLite.so");
     gSystem->Load("libDataFormatsPatCandidates.so");
     gROOT->SetStyle("Plain");
     gSystem->Load("libRooFit");
     using namespace RooFit;
-   }
+    }
+    */
 }
